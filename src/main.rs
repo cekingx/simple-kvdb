7e722c2108e072109568206016c4b2623232ba6d
Dirga Yasa <cekingx@gmail.com>
2023-07-21 23:25:31 +0800

    feat: listen tcp stream

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::{net::{TcpListener, TcpStream}, io::{BufReader, prelude::*}};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle(stream);
+    }
+}
+
+fn handle(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader
+        .lines()
+        .next();
+
+    println!("lines: {:?}", request_line);
+    stream.write_all("hello\n".as_bytes()).unwrap();
 }
