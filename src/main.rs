7127b27935df898dc166c459b29935b62346eccd
Dirga Yasa <cekingx@gmail.com>
2023-07-24 12:11:19 +0800

    feat: read all line
    - read all line until the end of stream
    - close connection after read

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{net::{TcpListener, TcpStream}, io::{BufReader, prelude::*}};
+use std::{net::{TcpListener, TcpStream}, io::{BufReader, prelude::*, BufWriter}, str};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -9,18 +9,32 @@ fn main() {
     }
 }
 
-fn handle(mut stream: TcpStream) {
-    let mut writer = stream.try_clone().unwrap();
-    let buf_reader = BufReader::new(&mut stream);
+// fn handle(mut stream: TcpStream) {
+//     let mut writer = stream.try_clone().unwrap();
+//     let buf_reader = BufReader::new(&mut stream);
 
+//     let response = format!(
+//         "HTTP/1.1 200 OK\r\n\
+//         Content-Length: 0\r\n\r\n\
+//         "
+//     );
+//     for line in buf_reader.lines() {
+//         println!("lines: {:?}", line);
+//         writer.write_all(response.as_bytes()).unwrap();
+//     }
+// }
+
+fn handle(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    let mut clone = stream.try_clone().unwrap();
+    let mut writer = BufWriter::new(&mut clone);
+    stream.read(&mut buffer).unwrap();
+    let x = str::from_utf8(&buffer).unwrap();
+    println!("{}", x);
     let response = format!(
         "HTTP/1.1 200 OK\r\n\
         Content-Length: 0\r\n\r\n\
         "
     );
-    for line in buf_reader.lines() {
-        println!("lines: {:?}", line);
-        writer.write_all(response.as_bytes()).unwrap();
-    }
-
+    writer.write_all(response.as_bytes()).unwrap();
 }
